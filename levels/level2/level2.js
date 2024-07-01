ae92dee6739ec764a61121a18ab67f99d881229b
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-07-01 12:08:46 +0300

    added the player2 character. added the physics of the player2. added the movment of the player2. added the points of player2. added the total score and the score of each player. edited the map.

--- levels/level2/level2.js
+++ levels/level2/level2.js
@@ -2,10 +2,16 @@ let player;
 let player2;
 let platforms;
 let cursors;
+let wad;
 let losingArea;
 let collectibles;
+let player2Collectibles;
 let score = 0;
 let scoreText;
+let player2Score = 0;
+let player2ScoreText;
+let player1ScoreText;
+let player1Score=0;
 
 class Level2 extends Phaser.Scene {
   constructor() {
@@ -15,21 +21,37 @@ class Level2 extends Phaser.Scene {
   preload() {
     this.load.image('character', '../../assets/charfire.png'); // Ensure this path is correct
     this.load.image('collectible', '../../assets/fireCollectible.png'); // Load the collectible image
+    this.load.image('player2Collectible', '../../assets/waterCollectible.png'); // Load the player2 collectible image
+    this.load.image('character2', '../../assets/blue_girl_character.png'); // Ensure this path is correct
   }
 
   create() {
     this.createBrickWall();
     this.createRoads();
 
+    // First Character
     player = this.physics.add.sprite(40, 550, 'character');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
+    // Second Character
+    player2 = this.physics.add.sprite(100, 550, 'character2');
+    player2.setBounce(0.2);
+    player2.setCollideWorldBounds(true);
+
     // Add player collision with platforms
     this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player2, platforms);
 
     // Create cursor keys input
     cursors = this.input.keyboard.createCursorKeys();
+
+    // Create W, A, D keys input
+    wad = {
+      up: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+      left: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+      right: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+    };
 
     // Create the losing area
     this.createLosingArea();
@@ -37,14 +59,24 @@ class Level2 extends Phaser.Scene {
     // Create collectibles
     this.createCollectibles();
 
-    // Add score text
-    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+    // Create player2 specific collectibles
+    this.createPlayer2Collectibles();
 
-    // Set up overlap between player and collectibles
+    // Add score text
+    scoreText = this.add.text(0, 0, 'Score: 0', { fontSize: '25px', fill: '#000' , fontStyle: 'bold'});
+    player1ScoreText = this.add.text(250, 0, 'Player1 Score: 0', { fontSize: '25px', fill: '#000', fontStyle: 'bold' });
+    player2ScoreText = this.add.text(550, 0, 'Player2 Score: 0', { fontSize: '25px', fill: '#000', fontStyle: 'bold' });
+
+    // Set up overlap between players and collectibles
     this.physics.add.overlap(player, collectibles, this.collectCollectible, null, this);
+   
+
+    // Set up overlap between player2 and player2 collectibles
+    this.physics.add.overlap(player2, player2Collectibles, this.collectPlayer2Collectible, null, this);
   }
 
   update() {
+    // Control for the first player
     if (cursors.left.isDown) {
       player.setVelocityX(-160); // Move left
     } else if (cursors.right.isDown) {
@@ -53,13 +85,26 @@ class Level2 extends Phaser.Scene {
       player.setVelocityX(0); // Stop
     }
 
-    // Allow the player to jump if they are touching the ground
     if (cursors.up.isDown && player.body.touching.down) {
       player.setVelocityY(-230); // Jump
     }
 
+    // Control for the second player
+    if (wad.left.isDown) {
+      player2.setVelocityX(-160); // Move left
+    } else if (wad.right.isDown) {
+      player2.setVelocityX(160); // Move right
+    } else {
+      player2.setVelocityX(0); // Stop
+    }
+
+    if (wad.up.isDown && player2.body.touching.down) {
+      player2.setVelocityY(-230); // Jump
+    }
+
     // Check for collision with the losing area
     this.physics.add.overlap(player, losingArea, this.handleLose, null, this);
+    this.physics.add.overlap(player2, losingArea, this.handleLose, null, this);
   }
 
   createRoads() {
@@ -73,18 +118,14 @@ class Level2 extends Phaser.Scene {
       { x: 730, y: 490, width: 70, height: 100 },
       { x: 250, y: 510, width: 180, height: 10 }, // girl start point
       { x: 0, y: 410, width: 300, height: 20 }, // above the girl
-      //   { x: 290, y: 430, width: 100, height: 10 }, // the pit the kills both
       { x: 380, y: 410, width: 300, height: 20 }, // beside the pit
-      //   { x: 490, y: 460, width: 80, height: 20 }, // second step of the stairs
-      //   { x: 440, y: 430, width: 80, height: 30 }, // 3rd step
-      //   { x: 550, y: 480, width: 80, height: 20 }, // 1st step
       { x: 0, y: 340, width: 50, height: 20 }, // the first block on the left to jump
       { x: 100, y: 300, width: 850, height: 23 }, // second floor
       { x: 750, y: 310, width: 40, height: 10 },
-      { x: 750, y: 170, width: 50, height: 20 }, // the block on the right to jump
+      { x: 750, y: 180, width: 50, height: 10 }, // the block on the right to jump
+      { x: 680, y: 250, width: 50, height: 10 },
       { x: 150, y: 220, width: 200, height: 20 }, // 3rd floor
       { x: 450, y: 220, width: 200, height: 20 }, // 3rd floor
-      //   { x: 0, y: 140, width: 50, height: 20 }, // the second block on the left side
       { x: 0, y: 100, width: 700, height: 30 }, // last floor
     ];
 
@@ -122,7 +163,7 @@ class Level2 extends Phaser.Scene {
 
   handleLose(player, losingArea) {
     // Handle what happens when the player loses
-    backToMenu();
+    backToLevels();
   }
 
   createBrickWall() {
@@ -148,9 +189,8 @@ class Level2 extends Phaser.Scene {
     const collectibleData = [
       { x: 200, y: 550 },
       { x: 300, y: 200 },
-      { x: 550, y: 400 },
+      { x: 550, y: 390 },
       { x: 400, y: 80 },
-      
     ];
 
     // Create collectibles at the specified positions
@@ -160,11 +200,41 @@ class Level2 extends Phaser.Scene {
     });
   }
 
+  createPlayer2Collectibles() {
+    player2Collectibles = this.physics.add.staticGroup(); // Create a static group for player2 collectibles
+
+    // Define the positions for the player2 collectibles
+    const player2CollectibleData = [
+      { x: 400, y: 500 },
+      { x: 150, y: 390 },
+      { x: 560, y: 80 },
+      { x: 500, y: 200 },
+    ];
+
+    // Create player2 collectibles at the specified positions
+    player2CollectibleData.forEach((data) => {
+      let player2Collectible = player2Collectibles.create(data.x, data.y, 'player2Collectible');
+      player2Collectible.setScale(0.5); // Adjust scale if necessary
+    });
+  }
+
   collectCollectible(player, collectible) {
     collectible.disableBody(true, true);
 
     // Add to score
     score += 1;
+    player1Score+=1;
+    player1ScoreText.setText('Player1 Score: ' + player1Score);
+    scoreText.setText('Score: ' + score);
+  }
+
+  collectPlayer2Collectible(player2, player2Collectible) {
+    player2Collectible.disableBody(true, true);
+
+    // Add to player2 score
+    score += 1;
+    player2Score += 1;
+    player2ScoreText.setText('Player2 Score: ' + player2Score);
     scoreText.setText('Score: ' + score);
   }
 }
@@ -192,5 +262,3 @@ function backToLevels() {
     game.destroy(true);
   }
 }
-
-// export default Level2;
